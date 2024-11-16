21bc9b672e205e08535f633238167013ef45c960
Ibrahim Bajwa <129992649+IbrahimBajwa313@users.noreply.github.com>
2024-11-16 22:11:47 +0500

    loader

---
+++ components/Loader.jsx
@@ -0,0 +1,10 @@
+const Loader = () => {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-green-500"></div>
+      </div>
+    );
+  };
+  
+  export default Loader;
+  
