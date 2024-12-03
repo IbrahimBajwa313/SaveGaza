9c30d1eca0947d720728a3e066f92943e36e48d5
Ibrahim Bajwa <129992649+IbrahimBajwa313@users.noreply.github.com>
2024-12-03 16:43:55 +0500

    hero banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import Wrapper from './Wrapper';
 
 const HeroBanner = () => {
     return (
-        <div className='z-0 w-screen'>
+        <div className='z-0 w-screen mt-2'>
+            <Wrapper>
             <Carousel autoPlay={true} infiniteLoop={true} showStatus={false} showThumbs={false}>
                 <div>
-                    <img src="hajj4.jpg" />
+                    <img src="/heroSlides/1.jpg" />
                    
                 </div>
                 <div>
-                    <img src="hajj3.jpg" />
-                    
+                    <img src="/heroSlides/2.jpg" />
+                   
                 </div>
                 <div>
-                    <img src="hajj5.jpg" />
-                    {/* <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.5] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
+                <img src="/heroSlides/3.jpg" />
+                {/* <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.5] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
                         Shop now
                     </div> */}
                 </div>
 
-            </Carousel></div>
+            </Carousel></Wrapper></div>
     )
 }
 
