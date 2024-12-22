d08c3ba00dbb4d9803a8f836171fe5bfb6e0a98f
Ibrahim Bajwa <129992649+IbrahimBajwa313@users.noreply.github.com>
2024-12-22 14:46:20 +0500

    as

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -75,6 +75,8 @@ export default function App({ Component, pageProps }) {
         href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap"
         rel="stylesheet"
       />
+      
+      <script src="//code.tidio.co/oa2rdanrhvguvw5ffgz012it3mptpgxn.js" async></script>
     </>
   );
 }
